f1ae9b5d09e73aa381425cf982bc54777f15e281
Polina Proshkina <polinaproshkina1998@gmail.com>
2023-12-05 12:04:55 +0100

    Does Task 1-3

---
+++ js/task-2.js
@@ -0,0 +1,9 @@
+'use strict';
+function getShippingMessage(country, price, deliveryFee) {
+    let totalPrice = price + deliveryFee;
+    let message = `Shipping to ${country} will cost ${totalPrice} credits`
+    return message;
+}
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20));
+console.log(getShippingMessage("Sweden", 100, 20)); 
